9bf58be1545f9470aaf6214992a0f60bdcd52548
dkipp <mail@daniel-kipp.de>
2018-09-09 21:26:43 +0200

    ui cleanup

--- src/store/profile/actions.ts
+++ src/store/profile/actions.ts
@@ -1,11 +1,12 @@
 import { ActionTree } from 'vuex';
+
 import axios from 'axios';
 import { ProfileState, User } from './types';
 import { RootState } from '../types';
 
 
 export const actions: ActionTree<ProfileState, RootState> = {
-    /*
+  /*
     fetchData({ commit }): any {
         axios({
             url: 'https://....',
@@ -18,4 +19,5 @@ export const actions: ActionTree<ProfileState, RootState> = {
         });
     },
     */
+
 };
